7ff4c287bca6edd5caa66e56efba9a7bb0cfe51c
Dung <dungphan16499@gmail.com>
2022-08-30 00:36:36 +0300

    Writing TB incr_byte_tb - NOT DONE

--- Incr_byte/tb/amm_pkg.sv
+++ Incr_byte/tb/amm_pkg.sv
@@ -1,5 +1,6 @@
 package amm_pkg;
 
+typedef logic [7:0] pkt_t [$];
 
 class amm_control_class #(
   parameter DATA_W   = 64,
@@ -7,19 +8,12 @@ class amm_control_class #(
   parameter BYTE_CNT = DATA_W/8
 );
 
+parameter BYTE_WORD = DATA_W/8;
+
 virtual avalon_mm_if #(
   .ADDR_WIDTH ( ADDR_W ),
   .DATA_WIDTH ( DATA_W )
 ) amm_if;
-
-// virtual amm_setting_if #(
-//   .ADDR_WIDTH ( ADDR_W )
-// ) setting_if;
-
-logic  [ADDR_W-1:0] base_addr;
-logic  [ADDR_W-1:0] length;
-// bit                 run;
-logic               waitrequest;
 
 function new( virtual avalon_mm_if #(
                                     .ADDR_WIDTH ( ADDR_W ),
@@ -28,84 +22,50 @@ function new( virtual avalon_mm_if #(
             );
 
 this.amm_if      = _amm_if;
-// this.setting_if = _setting_if;
-this.base_addr   = 'X;
-this.length      = 'X;
-this.waitrequest = 'X;
-// this.run        = 1'b0;
 
 endfunction
 
 `define cb @( posedge amm_if.clk );
 
-// task setting( input _base_addr,
-//                     _length
-//             );
-// forever
-//   begin
-//   // if( setting_if.waitrequest == 1'b0 )
-//     begin
-//       this.base_addr        = _base_addr;
-//       this.length           = _length;
-//       // this.run              = 1'b1;
-//       setting_if.base_addr <= _base_addr;
-//       setting_if.length    <= _length;
-//       setting_if.run       <= 1'b1;
-//     end
+task read_data( input pkt_t _read_data );
 
-//   `cb;
+logic              begin_reading;
+logic              rd_data_valid;
+int cnt_word;
+logic [DATA_W-1:0] pkt_data;
+int pkt_size;
 
-//   if( setting_if.run == 1'b1 )
-//     begin
-//       setting_if.run       <= 1'b0;
-//       setting_if.base_addr <= '0;
-//       setting_if.length    <= '0;
-//     end
-//   end
-
-// endtask
-
-task read();
-
-logic wait_rq;
-int cnt_length;
-int cnt_data;
-logic data_valid;
-
-bit begin_read_data;
-
-while( cnt_data < this.length )
+pkt_size = _read_data.size();
+// $display("size: %0d", pkt_size);
+while( cnt_word <= pkt_size/BYTE_WORD )
   begin
-    $display("length: %0d", this.length);
-    $display("cnt_data: %0d", cnt_data);
-    if( this.waitrequest == 1'b1 )
+    `cb;
+    if( amm_if.read == 1'b1 )
+      begin_reading = 1'b1;
+    // $display("1");
+    if( begin_reading == 1'b1 )
       begin
-        if( amm_if.address == this.length + this.base_addr )
-          amm_if.waitrequest <= 1'b0;
-        else
-          begin
-            wait_rq             = $urandom_range( 1,0 );
-            amm_if.waitrequest <= wait_rq;
-          end
-        if( amm_if.address == this.length + this.base_addr  && ( amm_if.read == 1'b0 ) && ( wait_rq == 1'b0 ) )
-          begin_read_data = 1'b1;
+        // $display("2");
+        amm_if.waitrequest   <= $urandom_range( 1,0 );
+        rd_data_valid         = $urandom_range( 1,0 );
+        amm_if.readdatavalid <= rd_data_valid;
 
-        if( begin_read_data == 1'b1 )
+        if( rd_data_valid == 1'b1 )
           begin
-            data_valid = $urandom_range( 1,0 ); 
-            amm_if.readdatavalid <= data_valid;
-            if( data_valid == 1'b1 )
-              begin 
-                amm_if.readdata <= $urandom_range( 2**DATA_W-1,0 );
-                cnt_data++;
+            // $display("3");
+            for( int j = (BYTE_WORD*cnt_word + BYTE_WORD) -1; j >= BYTE_WORD*cnt_word; j-- )
+              begin
+                pkt_data[7:0] = _read_data[j];
+                if( j != BYTE_WORD*cnt_word )
+                  pkt_data = pkt_data << 8;
               end
+            cnt_word++;
+            amm_if.readdata <= pkt_data;
+            // $display("data: %x", pkt_data );
           end
       end
-    `cb;
   end
-
-begin_read_data = 1'b0;
-
+  
 endtask
 
 endclass
--- Incr_byte/tb/byte_incr_tb.sv
+++ Incr_byte/tb/byte_incr_tb.sv
@@ -6,6 +6,7 @@ parameter int DATA_WIDTH_TB = 32; /////
 parameter int ADDR_WIDTH_TB = 10;
 parameter int BYTE_CNT_TB   = DATA_WIDTH_TB/8;
 
+parameter BYTE_WORD = DATA_WIDTH_TB/8;
 logic srst_i_tb;
 bit clk_i_tb;
 
@@ -38,10 +39,6 @@ avalon_mm_if #(
 ) amm_write_if (
   .clk ( clk_i_tb )
 );
-
-// amm_setting_if #( 
-//   .ADDR_WIDTH( ADDR_WIDTH_TB )
-// ) amm_set_if ();
 
 amm_control_class #(
   .DATA_W ( DATA_WIDTH_TB ),
@@ -94,16 +91,59 @@ forever
         run_i_tb       <= 1'b0;
         base_addr_i_tb <= 1'b0;
         length_i_tb    <= 1'b0;
-        // repeat(10)
-        //   @( posedge clk_i_tb );
-        // break;
       end
     @( posedge clk_i_tb );
 
   end
 
 endtask
- 
+
+task assert_wr_wait();
+
+@( posedge clk_i_tb )
+amm_write_if.waitrequest <= 1'b1;
+
+endtask
+
+task deassert_wr_wait();
+
+@( posedge clk_i_tb )
+amm_write_if.waitrequest <= 1'b0;
+
+endtask
+
+task assign_wr_wait_rq();
+
+forever
+  begin
+    @( posedge clk_i_tb );
+    repeat( 2 )
+      assert_wr_wait();
+    repeat( 2 )
+      deassert_wr_wait();
+  end
+
+endtask
+
+function automatic pkt_t gen_1_pkt ( int pkt_size );
+
+pkt_t       new_pkt;
+logic [7:0] gen_random_byte;
+
+for( int i = 0; i < pkt_size; i++ )
+  begin
+    gen_random_byte = $urandom_range( 2**8,0 );
+    new_pkt[i] = gen_random_byte;
+  end
+
+return new_pkt;
+
+endfunction
+
+int cnt_word;
+int int_part;
+int mod_part;
+
 initial
   begin
     srst_i_tb <= 1'b1;
@@ -114,81 +154,25 @@ initial
     srst_i_tb <= 1'b0;
     amm_write_if.waitrequest <= 1'b0;
        
-    // amm_read_data = new( amm_read_if );
+    amm_read_data = new( amm_read_if );
 
     base_addr = 10'h10;
     length    = 10'd15;
+    int_part  = length / BYTE_WORD;
+    mod_part  = length % BYTE_WORD;
+    cnt_word  = ( mod_part == 0 ) ? int_part : int_part + 1;
+    $display("cnt_word: %0d", cnt_word);
 
-    // amm_read_data.waitrequest = waitrequest_o_tb;
-    // amm_read_data.base_addr = base_addr;
-    // amm_read_data.length    = length;
     setting();
-    // @( posedge clk_i_tb );
-    
-    // amm_read_if.waitrequest <= 1'b1;
-    // @( posedge clk_i_tb );
-    // amm_read_if.waitrequest <= 1'b0;
+    fork
+      assign_wr_wait_rq();
+      amm_read_data.read_data( gen_1_pkt( 16 ) );
+    join_any
 
-    // repeat(5)
-    //   @( posedge clk_i_tb )
-    // while( waitrequest_o_tb != 1'b0 )
-    //   @( posedge clk_i_tb );
-    // ##6;
-    // @( posedge clk_i_tb );
-    amm_read_if.readdatavalid <= 1'b1;
-    amm_read_if.readdata <= 32'hd0;
-
-    // @( posedge clk_i_tb );
-    // amm_read_if.readdatavalid <= 1'b0;
-    // // amm_read_if.readdata <= 64'd0;
-
-    // @( posedge clk_i_tb );
-    @( posedge clk_i_tb );
-    amm_read_if.readdatavalid <= 1'b1;
-    amm_read_if.readdata <= 32'hd1;
- 
-    // @( posedge clk_i_tb );
-    // amm_read_if.readdatavalid <= 1'b0;
-    // // amm_read_if.readdata <= 64'd0;
-
-    // @( posedge clk_i_tb );
-    @( posedge clk_i_tb );
-    amm_read_if.readdatavalid <= 1'b1;
-    amm_read_if.readdata <= 32'hd2;
-
-    // @( posedge clk_i_tb );
-    // amm_read_if.readdatavalid <= 1'b0;
-    // // amm_read_if.readdata <= 64'd0;
-
-    // @( posedge clk_i_tb );
-    @( posedge clk_i_tb );
-    amm_read_if.readdatavalid <= 1'b1;
-    amm_read_if.readdata <= 32'hd3;
-
-    // @( posedge clk_i_tb );
-    // amm_read_if.readdatavalid <= 1'b0;
-    // // amm_read_if.readdata <= 64'd0;
-
-    // @( posedge clk_i_tb );
-    @( posedge clk_i_tb );
-    amm_read_if.readdatavalid <= 1'b1;
-    amm_read_if.readdata <= 32'hd4;
-
-    // @( posedge clk_i_tb );
-    // amm_read_if.readdatavalid <= 1'b0;
-    // // amm_read_if.readdata <= 64'd0;
- 
-    // @( posedge clk_i_tb );
-    @( posedge clk_i_tb );
-    amm_read_if.readdatavalid <= 1'b1;
-    amm_read_if.readdata <= 64'd45;
-
-    repeat(5)
+    while( !( ( amm_write_if.address == base_addr + cnt_word -1 ) && amm_write_if.write && amm_write_if.waitrequest == 1'b0 ) )
       @( posedge clk_i_tb );
-    // amm_read_if.readdatavalid <= 1'b0;
-    // amm_read_if.readdata <= 64'd0;
 
-    //   amm_read_data.read();
+    @( posedge clk_i_tb );
 
 
     $stop();
